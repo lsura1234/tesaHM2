1950a936962f1098becb38c08230399e994a4e2c
Suradech <allee2470@gmail.com>
2019-01-09 14:34:22 +0700

    create project sucess

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
-//var path = require('path');
+app = express();
+//var  = require('path');
 //var favicon = require('serve-favicon');
 //var logger = require('morgan');
 //var cookieParser = require('cookie-parser');
