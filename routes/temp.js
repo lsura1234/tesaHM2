f1b17f0b3f46026976782a551c9f958624eb9d16
bibim18 <namfon_inuyasha@hotmail.com>
2019-01-09 16:39:50 +0700

    get data

--- routes/temp.js
+++ routes/temp.js
@@ -4,16 +4,16 @@ var temp = require("../models/temp");
 
 router.post("/receiveData", function (req, res) {
     var data = req.body
-    var Temp = new temp(req.body);
-    Temp.save(function(err) {
-        if(err) {
-          console.log(err);
-          res.render("../views/employees/create");
-        } else {
-          console.log("Successfully created an tmep.");
-          //res.redirect("/employees/show/"+employee._id);
-        }
-      });
+    // var Temp = new temp(data);
+    // Temp.save(function(err) {
+    //     if(err) {
+    //       console.log(err);
+    //       // res.render("../views/employees/create");
+    //     } else {
+    //       console.log("Successfully created an tmep.");
+    //       //res.redirect("/employees/show/"+employee._id);
+    //     }
+    //   });
     //console.log(data)
     res.send(data);
     
