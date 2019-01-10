95416ac5e62e44b0ab27b1f0b0cbca59ced29924
bibim18 <namfon_inuyasha@hotmail.com>
2019-01-10 15:25:50 +0700

    add moment time plus 7

--- models/beacon.js
+++ models/beacon.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
+var moment = require('moment')
 
 var BeaconSchema = new mongoose.Schema({
   "P-IN": Number,
   "P-OUT": Number,
-  Timestamp: { type : Date, default: Date.now }
+  Timestamp: { type : Date, default: moment().add('7','hours')}
 });
 
 module.exports = mongoose.model('BeaconData', BeaconSchema);
--- models/sensor.js
+++ models/sensor.js
@@ -5,7 +5,7 @@ var SensorSchema = new mongoose.Schema({
   Humidity: Number,
   "P-IN": Number,
   "P-OUT": Number,
-  Timestamp: { type : Date, default: Date.now }
+  Timestamp: { type : Date, default: moment().add('7','hours') }
 });
 
 module.exports = mongoose.model('SensorData', SensorSchema);
