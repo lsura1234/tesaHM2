3141e90e98c279ac540728b5d35c00be857ab032
bibim18 <namfon_inuyasha@hotmail.com>
2019-01-10 14:07:46 +0700

    insert data from sensor into mongo

--- routes/hackAthon/line.js
+++ routes/hackAthon/line.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var beacon = require("../../models/beacon");
 
 router.post("/putSanam", async function (req, res) {
-  // var data = req.body
+  var data = req.body
   // var id = data.DevEUI_uplink.payload_parsed.frames[0].value
   // var temp = data.DevEUI_uplink.payload_parsed.frames[1].value
   // var dataHW = await temp.create({teamID: id, temp})
-  // res.send(dataHW);
+  res.send(data);
   res.end()
 })
 
--- routes/hackAthon/sensor.js
+++ routes/hackAthon/sensor.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var sensor = require("../../models/sensor");
 
 router.post("/putSanam", async function (req, res) {
-  // var data = req.body
-  // var id = data.DevEUI_uplink.payload_parsed.frames[0].value
-  // var temp = data.DevEUI_uplink.payload_parsed.frames[1].value
-  // var dataHW = await temp.create({teamID: id, temp})
-  // res.send(dataHW);
+  var data = req.body
+  var Temperature = 0
+  var Humidity = 0
+  data.data.DevEUI_uplink.payload_parsed.frames.map(data => {
+    if(data.typeString === 'Temperature Sensor'){
+      Temperature = data.value
+    } else if(data.typeString === 'Humidity Sensor'){
+      Humidity = data.value
+    }
+  })
+
+  var dataHW = await sensor.create({Temperature,Humidity})
+  res.send(dataHW);
   res.end()
 })
 
-router.get("/predict", async function (req, res) {
-  // var data = req.body
-  // var id = data.DevEUI_uplink.payload_parsed.frames[0].value
-  // var temp = data.DevEUI_uplink.payload_parsed.frames[1].value
-  // var dataHW = await temp.create({teamID: id, temp})
-  // res.send(dataHW);
-  res.end()
-})
 module.exports = router;
